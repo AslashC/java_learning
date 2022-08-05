59b8d03bda3ad16f2a961a71fa72ff4fbced2cff
chenhongying <allenchanhy@gmail.com>
2022-08-05 09:04:26 +0800

    My first java programme

---
+++ java-foundational/src/main/java/net/chy/java/foundational/object/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.chy.java.foundational.object;
+
+/**
+ * My First Class in IDEA
+ *
+ * @author CHY gmail.com
+ * since JDK 11
+ * @version 2022/8/4 17:39
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld with Win10 & JDK11 &Gut &Maven &IDEA2022");
+    }
+}
